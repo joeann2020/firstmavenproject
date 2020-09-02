8e271128fbdef3830201069dcc21791a4f48b107
Joyce Anthony <jobsforjoyce05@gmail.com>
2020-09-02 11:09:31 -0500

    intial commit

---
+++ FirstMavenProject/src/main/java/com/joe/techie/FirstMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.joe.techie.FirstMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
